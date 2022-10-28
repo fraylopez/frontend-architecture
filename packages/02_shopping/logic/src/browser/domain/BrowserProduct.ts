e5ee6b1a530d9c2bfcf4af1e3d91792061be2e4d
martiriera <martiriera@github.com>
2022-10-28 17:37:16 +0200

    chore: anemic product browser model

--- packages/02_shopping/logic/src/browser/domain/BrowserProduct.ts
+++ packages/02_shopping/logic/src/browser/domain/BrowserProduct.ts
@@ -1,17 +1,7 @@
-import { observable } from "mobx";
 import { Product } from "../../_shared/domain/Product";
-import { IProduct } from "../../_shared/infrastructure/BackendSimulator";
-
 export class BrowserProduct extends Product {
-  @observable
-  readonly stock: number;
-
-  constructor(readonly id: string, name: string, price: number, stock: number) {
-    super(id, name, price);
-    this.stock = stock;
-  }
-
-  static fromPrivitives(product: IProduct): BrowserProduct {
-    return new BrowserProduct(product.id, product.name, product.price, product.stock)
+  // TODO: Anemic Domain Model
+  constructor(id: string, name: string, price: number, stock: number) {
+    super(id, name, price, stock);
   }
 }
