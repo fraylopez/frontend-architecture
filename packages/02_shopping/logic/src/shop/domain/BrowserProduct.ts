153b58a0d055a948224fe574029c7579dac3fa17
David Valentín <davidvalentinsemail@gmail.com>
2022-11-11 18:02:25 +0100

    remove unused files

--- packages/02_shopping/logic/src/shop/domain/BrowserProduct.ts
+++
@@ -1,7 +0,0 @@
-import { Product } from "../../_shared/domain/Product";
-export class BrowserProduct extends Product {
-  // TODO: Anemic Domain Model
-  constructor(id: string, name: string, price: number, stock: number) {
-    super(id, name, price, stock);
-  }
-}
